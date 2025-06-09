613f24fb2298ea830eedd347252b131ea1a7d8e4
Anish Kumar <35196480+anish9696@users.noreply.github.com>
2025-06-09 11:00:07 +0530

    Episode 2 commit

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I m an h1 tag"),
-    React.createElement("h2", {}, "I m an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I m an h1 tag"),
-    React.createElement("h2", {}, "I m an h2 tag"),
-  ]),
+  React.createElement(
+    "div",
+    { id: "child", key: "child1" }, // ✅ add key here
+    [
+      React.createElement(
+        "h1",
+        { id: "hello", key: "h1tag" },
+        "This is Hello React"
+      ),
+      React.createElement(
+        "h2",
+        { id: "hello2", key: "h2tag" },
+        "This is Hello React 2"
+      ),
+    ]
+  ),
 ]);
-console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
 root.render(parent);
