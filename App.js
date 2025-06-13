a83289a8005bde86e19879c9471f2652cc738a68
Anish Kumar <35196480+anish9696@users.noreply.github.com>
2025-06-13 21:09:51 +0530

    Episode commit 4

--- App.js
+++ App.js
@@ -1,39 +1,111 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import MyLogo from "./MyLogo.png";
+/**
+ * Food ordering company
+ * ANNService
+ * App Layout
+ * Header
+ *    logo
+ *    Nav item(Home, aboutUs, cart, links)
+ * Body
+ *    searchComponent, Restaurant component(Restaurant card), FoodComponent (Food Card)
+ *    Restaurant card
+ *      - Img
+ *      -  Name of Res, Star Rating, cuisine, delivery time
+ * Footer
+ *  copyright
+ *  links
+ *  Address
+ *  Contact
+ */
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello React ");
-// console.log(heading);
-const elem = <span>React Element</span>;
-
-const Title = () => (
-  <h1 className="head" tabIndex="5" id="heading">
-    {elem} Hello React using JSX
-  </h1>
-);
-
-const title = <h1 className="newWay">Hello World from titlle</h1>;
-// console.log(jsxHeading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const HeadingComponent2 = () => (
-  <h1 className="anish"> Hello React new Way </h1>
-);
-
-console.log(HeadingComponent2);
-const number = 1000;
-
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div>
-      <Title />
-      <h2>{number}</h2>
-      {title}
-      <h1>Hello React Functional component</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart </li>
+        </ul>
+      </div>
     </div>
   );
 };
 
-console.log(HeadingComponent);
-// root.render(heading);
-// root.render(heading);
-root.render(<HeadingComponent />);
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="cholla-res"
+        alt="cholla-res"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Chole%20Bhature.png"
+      ></img>
+      <h3>Anish Foods</h3>
+      <h4>cholla bhature, north india, asian</h4>
+      <h4>4.4 stars</h4>
+      <h4> 38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
